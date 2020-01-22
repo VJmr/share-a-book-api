ba23e83ae85de7bdb7a8b0db95826af1b9e3425b
VJmr <jaganvudumula@gmail.com>
2020-01-22 14:58:01 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var cors = require("cors");
+
+const app = express();
+const bodyParser = require("body-parser");
+
+var PropertiesReader = require("properties-reader");
+var properties = PropertiesReader("server.properties");
+
+const port = properties.get("server.port.number");
+const serverName = properties.get("server.port.name");
+
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    next();
+});
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+app.use(bodyParser.json());
+
+app.listen(port, serverName, function() {
+    console.log("Server is running on " + port + " port");
+});
